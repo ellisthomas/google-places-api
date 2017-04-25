4f2db5101de874498f4b5da754923e8b201632bd
ellisthomas <ellisthomas80@gmail.com>
2017-04-24 23:13:12 -0500

    printing to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
 	
-const apiKey = "";
+const apiKey = "";   //api key <=
+
+let placesNames;
 
 $("body").on("click", "li", (e) => {
 	console.log(e.target.innerHTML);
-	loadPlaces(e.target.innerHTML).then((data) =>{
+	loadPlaces(e.target.innerHTML).then((data) => {
+		placesNames = data.results;
 		console.log(data);
+		placesNames.forEach((each) => {
+			let domString = `<p>${each.name}</p>`;
+		$("#placeName").append(domString);
+		})
 	}).catch((error) => {
 		console.log(error);
 	});
 });
 
 
+
 const loadPlaces = (dropdownType) => {
-		return new Promise ((resolve, reject) => {
+		return new Promise((resolve, reject) => {
 			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.1627,-86.7816&radius=50000&type=${dropdownType}&key=${apiKey}`)
 		.done((data) => resolve(data))
 		.fail((error) => reject(error));
@@ -21,7 +29,6 @@ const loadPlaces = (dropdownType) => {
 		});
 		
 	};
-
 
 
 });
