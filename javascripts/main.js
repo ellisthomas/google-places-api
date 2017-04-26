d196c4d8386a4518a57fc87f453b6d90e7fb3f0e
ellisthomas <ellisthomas80@gmail.com>
2017-04-25 19:27:52 -0500

    in class work complete

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,33 +2,76 @@ $(document).ready(function() {
 	
 const apiKey = "";   //api key <=
 
-let placesNames;
-
-$("body").on("click", "li", (e) => {
-	console.log(e.target.innerHTML);
-	loadPlaces(e.target.innerHTML).then((data) => {
-		placesNames = data.results;
-		console.log(data);
-		placesNames.forEach((each) => {
-			let domString = `<p>${each.name}</p>`;
-		$("#placeName").append(domString);
-		})
-	}).catch((error) => {
-		console.log(error);
-	});
-});
 
 
-
-const loadPlaces = (dropdownType) => {
-		return new Promise((resolve, reject) => {
-			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.1627,-86.7816&radius=50000&type=${dropdownType}&key=${apiKey}`)
-		.done((data) => resolve(data))
-		.fail((error) => reject(error));
-
+	$("body").on("click", "li", (e) => {
+		console.log(e.target.innerHTML);
+		loadPlaces(e.target.innerHTML).then((results) => {
+			// console.log(data);
+			writePlaceToDom(results);
+		}).catch((error) => {
+			console.log(error);
 		});
-		
-	};
+	});
+
+	$("body").on("click", ".place", (e) => {
+		let place_id = e.target.id;
+		loadDetails(place_id).then((result) => {
+			console.log("result", result);
+			writeAddressToDom(result.formatted_address);
+		});
+
+	});
+
+
+	const loadDetails = (place_id) => {
+		return new Promise((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${apiKey}`)
+			.done((data) => resolve(data.result))
+			.fail((error) => reject(error));
+		});
+	}
+
+	const loadPlaces = (dropdownType) => {
+			return new Promise((resolve, reject) => {
+				$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.1627,-86.7816&radius=50000&type=${dropdownType}&key=${apiKey}`)
+			.done((data) => resolve(data.results))
+			.fail((error) => reject(error));
+			});
+			
+		};
+
+	const writeAddressToDom = (address) => {
+		let outputString = `<div>${address}</div>`;
+		$("#addresses").append(outputString);
+	}	
+
+	const writePlaceToDom = (results) => {
+		let outputString = "";
+		for(let i = 0; i < results.length; i++) {
+			outputString += `<a href"#"><div id="${results[i].place_id}" class="place">${results[i].name}</div></a>`;
+		}
+		$("#input").html(outputString);
+	}	
+
 
 
 });
+
+// let placesNames;
+
+// $("body").on("click", "li", (e) => {
+// 	console.log(e.target.innerHTML);
+// 	loadPlaces(e.target.innerHTML).then((data) => {
+// 		placesNames = data.results;
+// 		console.log(data);
+// 		placesNames.forEach((each) => {
+// 			let domString = `<p>${each.name}</p>`;
+// 		$("#placeName").append(domString);
+// 		})
+// 	}).catch((error) => {
+// 		console.log(error);
+// 	});
+// });
+
+
